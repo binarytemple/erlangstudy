40277a2338147dc89638905180511fe3fd34ea92
bryan hunt <github@binarytemple.co.uk>
2014-05-28 22:35:00 +0200

    io and inter-module calling

--- 2014-5-9/src/second.erl
+++ 2014-5-9/src/second.erl
@@ -10,16 +10,10 @@
 -author("bryan").
 
 %% API
--export([env_get_var/1]).
+-export([start/0]).
 
-env_get_var(Key) ->
-Tokens = [ string:tokens(Env,"=") || Env <- os:getenv(),
-  ( case string:tokens(Env,"=")  of [Key,_] -> true;  _ -> false end )
-],
-  case Tokens of [[_,Name]|_]-> Name; _ -> {error,var_not_found,Tokens} end.
+start() ->
+  io:fwrite("Hello world ~p !", [
 
-
-%% case Env of ["USER",Value] -> Value  ; _ -> false
-%% end],
-%%Res  .
-  %%case Res of [[Key|Val]] -> Val; Other -> {fail,not_found,Other} end.
+     first:foo("foo")
+  ]).
